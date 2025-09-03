4b327c1f84554d2a95f9eb3838973fa0308546bf
Manuel Wedler <manuel@wedler.dev>
2025-09-03 12:21:03 +0200

    Add signature-util for extracting SignatureData from ABIs

---
+++ services/server/src/server/services/utils/signature-util.ts
@@ -0,0 +1,33 @@
+import { Interface, keccak256, Fragment, JsonFragment } from "ethers";
+
+export interface SignatureData {
+  signature: string;
+  signatureHash32: string;
+  signatureType: "function" | "event" | "error";
+}
+
+export function extractSignaturesFromAbi(abi: JsonFragment[]): SignatureData[] {
+  const signatures: SignatureData[] = [];
+
+  const iface = new Interface(abi);
+
+  iface.fragments.forEach((fragment) => {
+    switch (fragment.type) {
+      case "function":
+      case "event":
+      case "error":
+        signatures.push(getSignatureData(fragment));
+    }
+  });
+
+  return signatures;
+}
+
+function getSignatureData(fragment: Fragment): SignatureData {
+  const signature = fragment.format("sighash");
+  return {
+    signature,
+    signatureHash32: keccak256(signature),
+    signatureType: fragment.type as "function" | "event" | "error",
+  };
+}
