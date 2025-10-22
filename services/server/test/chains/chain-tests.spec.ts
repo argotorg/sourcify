93e50c6def7a87e42b7b30e0cabd307a45a1eebb
pepihasenfuss <one@ungravel.com>
2025-10-22 10:03:06 +0200

    fix link

--- services/server/test/chains/chain-tests.spec.ts
+++ services/server/test/chains/chain-tests.spec.ts
@@ -1922,16 +1922,16 @@ describe("Test Supported Chains", function () {
     "PlatON Mainnet",
     "shared/",
   );
-  
+
   // soneium Mainnet
   verifyContract(
     "0xcA11bde05977b3631167028862bE2a173976CA11",
     "1868",
     "Soneium Mainnet",
     "multicall-src/",
-    "partial"
+    "partial",
   );
-  
+
   // WorldChain
   verifyContract(
     "0xcA11bde05977b3631167028862bE2a173976CA11",
