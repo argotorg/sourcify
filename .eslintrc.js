23871847bc2c67d0395278f518426360ca596a80
Yury Akudovich <yorik13@gmail.com>
2025-10-28 09:35:27 +0100

    feat: Add SSL support for SOURCIFY_POSTGRES (#2458)
    * feat: Add SSL support for SOURCIFY_POSTGRES
    
    Add `SOURCIFY_POSTGRES_SSL` to enable/disable SSL.
    Add `SOURCIFY_POSTGRES_SSL_REJECT_UNAUTHORIZED` to verify server certificate.
    
    * Fixed lint errors
    
    * More reformatting
    
    * Prettier fix
    
    * Fixing indent
    
    * More lint fixes
    
    * Remove conflicting eslint indent rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,6 @@ module.exports = {
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["warn"],
-    indent: [
-      "error",
-      2,
-      { SwitchCase: 1, ignoredNodes: ["ConditionalExpression"] },
-    ],
     "@typescript-eslint/ban-ts-comment": "off",
   },
   env: {
