94eb742b2ca7cb877a7be6b802991d2cfe7626d2
Marco Castignoli <marco@argot.org>
2025-10-30 14:26:15 +0100

    Fix tests

--- services/server/test/integration/apiv2/verification/verify.similarity.spec.ts
+++ services/server/test/integration/apiv2/verification/verify.similarity.spec.ts
@@ -3,9 +3,13 @@ import chaiHttp from "chai-http";
 import sinon from "sinon";
 import { LocalChainFixture } from "../../../helpers/LocalChainFixture";
 import { ServerFixture } from "../../../helpers/ServerFixture";
-import { hookIntoVerificationWorkerRun } from "../../../helpers/helpers";
+import {
+  hookIntoVerificationWorkerRun,
+  waitSecs,
+} from "../../../helpers/helpers";
 import { SourcifyDatabaseService } from "../../../../src/server/services/storageServices/SourcifyDatabaseService";
 import { MockVerificationExport } from "../../../helpers/mocks";
+import { assertJobVerification } from "../../../helpers/assertions";
 
 chai.use(chaiHttp);
 
@@ -74,19 +78,16 @@ describe.only("POST /v2/verify/similarity/:chainId/:address", function () {
     chai.expect(verifyRes.status).to.equal(202);
     chai.expect(verifyRes.body).to.have.property("verificationId");
 
-    await resolveWorkers();
+    // We need to wait because `getSimilarityCandidatesByRuntimeCode` is delaying the job push to the queue
+    await waitSecs(1);
 
-    const jobRes = await chai
-      .request(serverFixture.server.app)
-      .get(`/v2/verify/${verifyRes.body.verificationId}`);
-
-    chai.expect(jobRes.status).to.equal(200);
-    chai.expect(jobRes.body.isJobCompleted).to.be.true;
-    chai.expect(jobRes.body.error).to.be.undefined;
-    chai.expect(jobRes.body.contract).to.deep.include({
-      chainId: chainFixture.chainId,
-      address: chainFixture.defaultContractAddress.toLowerCase(),
-      match: "exact_match",
-    });
+    await assertJobVerification(
+      serverFixture,
+      verifyRes,
+      resolveWorkers,
+      chainFixture.chainId,
+      chainFixture.defaultContractAddress,
+      "exact_match",
+    );
   });
 });
