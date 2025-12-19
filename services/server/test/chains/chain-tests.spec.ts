69c23037d1307e0b4eeeb591d85b12799616257e
Manuel Wedler <manuel@wedler.dev>
2025-12-19 15:31:58 +0100

    Fix linting

--- services/server/test/chains/chain-tests.spec.ts
+++ services/server/test/chains/chain-tests.spec.ts
@@ -1994,14 +1994,14 @@ describe("Test Supported Chains", function () {
     "0xf6eacb6e11b66ec61822fe8097c4ba9b12410f8f",
     "1328",
     "Sei Testnet",
-    "shared/"
+    "shared/",
   );
   // Sei Network
   verifyContract(
     "0xc9f275dc9c9d7451dAa5655d0105F0266b31347f",
     "1329",
     "Sei Network",
-    "shared/"
+    "shared/",
   );
 
   // Tempo Testnet (Andantino)
